8c91abe21190eaa4bd7e3cf3a0a4a3b4c9986d09
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-03 19:58:17 -0400

    installed all needed npms and required them in the server.js

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# employee_tracker-hw_u12-mysql
+# Employee Tracker
---
+++ server.js
@@ -0,0 +1,4 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require("console.table");
+const fs = require("fs");
