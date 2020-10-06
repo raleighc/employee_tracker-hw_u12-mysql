b2e8bcfda4a103ece2b23647052b9a39da75407f
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-06 02:10:50 -0400

    reverted back to only using one file and abandoned the async await files because I couldt problem solve the errors. wrote the function to print employees based on department. Almost have the add new employee function working.

--- db.js
+++ db.js
@@ -2,33 +2,59 @@ const mysql = require("mysql");
 const util = require("util");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "NewRalE88?",
-    database: "tracker_db"
-  });
-  
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "NewRalE88?",
+  database: "tracker_db",
+});
+
 connection.connect();
 connection.query = util.promisify(connection.query);
 
 class DB {
-    constructor(connection) {
-        this.connection = connection
-    }
-    viewAllEmployees(){
-        console.log("")
-        return this.connection.query('SELECT employees.first_name, role.title, department.name, concat(managers.first_name , " " , managers.last_name) AS "manager" FROM employees LEFT JOIN employees AS managers ON employees.manager_id=managers.emp_id INNER JOIN role ON employees.role_id=role.role_id INNER JOIN department ON role.dept_id=department.dept_id;')
-    }
-    endConnection(){
-        console.log("...your connection has been severed.");
-        this.connection.end();
-    }
+  constructor(connection) {
+    this.connection = connection;
+  }
+  viewAllEmployees() {
+    console.log("");
+    return this.connection.query(
+      `SELECT employees.emp_id, employees.first_name, employees.last_name, role.title,
+      department.name, role.salary,
+      concat(managers.first_name , " " , managers.last_name) AS "manager"
+      FROM employees LEFT JOIN employees AS managers ON employees.manager_id=managers.emp_id
+      INNER JOIN role ON employees.role_id=role.role_id
+      INNER JOIN department ON role.dept_id=department.dept_id ORDER BY employees.emp_id asc;`
+    );
+  }
+  allDeptNames() {
+      return this.connection.query(`SELECT name FROM department;`, (err, result) =>{
+          if(err) throw err;
+          const departmentsArray = [];
+          for (let i = 0; i < result.length; i++) {
+              departmentsArray.push(result[i].department);
+          }
+      });
+  }
+  viewByDept() {
+    console.log("");
+    return this.connection.query(
+      `SELECT employees.emp_id, employees.first_name, employees.last_name, role.title
+        FROM employees
+        INNER JOIN role ON employees.role_id=role.role_id
+        INNER JOIN department ON role.dept_id=department.dept_id
+        WHERE department.name = ?;`
+    );
+  }
+  endConnection() {
+    console.log("...your connection has been severed.");
+    this.connection.end();
+  }
 }
 module.exports = new DB(connection);
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const inquirer = require("inquirer");
+const mysql = require("mysql");
 const cTable = require("console.table");
 const fs = require("fs");
-const db = require("./db.js");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "NewRalE88?",
+  database: "tracker_db",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  begin();
+});
 
 function begin() {
   inquirer
@@ -26,50 +46,116 @@ function begin() {
     .then(function (answer) {
       if (answer.iWouldLikeTo === "View All Employees") {
         viewAllEmployees();
+      } else if (answer.iWouldLikeTo === "View All Employees By Department") {
+        viewByDept();
+      } else if (answer.iWouldLikeTo === "Add Employee") {
+        addEmployee();
       } else if (answer.iWouldLikeTo === "Exit") {
         endConnection();
       }
     });
 }
-async function findAllEmployees() {
-  const allEmployees = await db.viewAllEmployees();
-  console.table(allEmployees);
+
+function viewAllEmployees() {
+  console.log("");
+  connection.query(
+    `SELECT employees.emp_id, employees.first_name, employees.last_name, role.title,
+      department.name, role.salary,
+      concat(managers.first_name , " " , managers.last_name) AS "manager"
+      FROM employees LEFT JOIN employees AS managers ON employees.manager_id=managers.emp_id
+      INNER JOIN role ON employees.role_id=role.role_id
+      INNER JOIN department ON role.dept_id=department.dept_id ORDER BY employees.emp_id asc;`,
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      begin();
+    }
+  );
+}
+
+function viewByDept() {
+  console.log("");
+  connection.query(`SELECT name FROM department;`, (err, result) => {
+    if (err) throw err;
+    const departmentsArray = [];
+    for (let i = 0; i < result.length; i++) {
+      departmentsArray.push(result[i]);
+      console.log(result[i])
+    }
+    inquirer
+      .prompt({
+        name: "department",
+        type: "list",
+        message: "Which department would you like to see employees for?",
+        choices: departmentsArray,
+      })
+      .then(({ department }) => {
+        // console.log(department);
+        connection.query(
+          `SELECT employees.emp_id, employees.first_name, employees.last_name, role.title
+                  FROM employees
+                  INNER JOIN role ON employees.role_id=role.role_id
+                  INNER JOIN department ON role.dept_id=department.dept_id
+                  WHERE department.name = ?;`,
+          [department],
+          (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            begin();
+          }
+        );
+      });
+  });
+}
+
+function addEmployee() {
+    
+    connection.query(`SELECT title FROM role;`, (err, result) => {
+        if (err) throw err;
+        const rolesArray = [];
+        for (let i = 0; i < result.length; i++) {
+          rolesArray.push(result[i].title);
+        }
+    inquirer.prompt([
+        {
+            name: "first",
+            type: "input",
+            message: "What is the employee's first name?"
+        },
+        {
+            name: "last",
+            type: "input",
+            message: "What is the employee's last name?"
+        },
+        {
+            name: "jobTitle",
+            type: "list",
+            message: "What is the employee's role?",
+            choices: rolesArray,
+        },
+    ]).then((info) => {
+        const associatedID = connection.query(`SELECT role-id FROM role WHERE title = ${info.jobTitle};`, (err, res) =>{
+            if (err) throw err;
+            console.log(res)
+        })
+        connection.query(
+            `INSERT INTO employees (first_name, last_name, role_id)
+            VALUES (?, ?, ?);`,
+            [info.first, info.last, associatedID],
+            (err, res) => {
+              if (err) throw err;
+              console.table(res);
+              viewAllEmployees();
+              begin();
+            }
+          );
+    })
+});
 }
 
 
-async function endConnection() {
-    const end = await db.endConnection();
-    end;
-  }
-  
 
-
-// begin();
-// function readDepartment() {
-//   console.log("Selecting all departments...\n");
-//   connection.query("SELECT * FROM department", function (err, res) {
-//     if (err) throw err;
-//     // Log all results of the SELECT statement
-//     console.table(res);
-//   });
-// }
-
-//   function readRole() {
-//     console.log("Selecting all roles...\n");
-//     connection.query("SELECT * FROM role", function(err, res) {
-//       if (err) throw err;
-//       // Log all results of the SELECT statement
-//       console.table(res);
-//     });
-//   }
-
-//   function readEmployee() {
-//     console.log("Selecting all employees...\n");
-//     connection.query("SELECT * FROM employees", function(err, res) {
-//       if (err) throw err;
-//       // Log all results of the SELECT statement
-//       console.table(res);
-//       endConnection();
-//     });
-//   }
-
+function endConnection() {
+  console.log("...your connection has been severed.");
+  connection.end();
+}
